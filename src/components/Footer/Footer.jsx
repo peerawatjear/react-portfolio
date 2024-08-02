d6b86a7f312f877f7463da390feef4f8ce0e6a13
PeerawatJearjamroon <peerawat2626@gmail.com>
2024-08-03 00:27:03 +0700

    footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -3,8 +3,8 @@ import styles from "./Footer.module.css";
 
 function Footer() {
   return (
-    <footer>
-        {/* <p>Copyright © 2023. All Rights Reserved.</p> */}
+    <footer className={styles.footer}>
+        <p>Copyright © 2023. All Rights Reserved.</p>
         <p>© 2024 MyWebsite.</p>
     </footer>
   )
